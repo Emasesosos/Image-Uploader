510186e9fd7170a2e7ccb923d7a4dc7fb95fc687
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-10-27 10:54:09 -0600

    Frontend listo: Html y CSS

--- image-uploader-app/src/ImageUploader.js
+++ image-uploader-app/src/ImageUploader.js
@@ -1,12 +1,14 @@
 import React from 'react';
-//import { UploadImage } from './components/UploadImage';
+import { UploadImage } from './components/UploadImage';
 import { Uploading } from './components/Uploading';
+import { UploadSuccess } from './components/UploadSuccess';
 
 export const ImageUploader = () => {
     return (
         <div className="container">
-            {/* <UploadImage /> */}
+            <UploadImage />
             <Uploading />
+            <UploadSuccess />
         </div>
     );
 };
---
+++ image-uploader-app/src/components/UploadSuccess.js
@@ -0,0 +1,45 @@
+import React, { useRef } from 'react';
+import checkCircle from './../assets/img/check_circle.svg';
+import interestelar from './../assets/img/interestelar.jpg';
+
+export const UploadSuccess = () => {
+
+    const refInput = useRef('');
+
+    const handleCopyLink = () => {
+
+        // console.log(refInput.current.valueOf().value);
+        refInput.current.select();
+        document.execCommand("copy");
+
+    };
+
+    return (
+
+        <div className="upload-success">
+            <div className="success-icono">
+                <img src={ checkCircle } alt="checkCircle"/>
+            </div>
+            <div className="success-title">
+                <p>Upload Successfully!</p>
+            </div>
+            <div className="success-img">
+                <img className="upload-image" src={interestelar} alt="interestelar"/>
+            </div>
+            <div className="success-link">
+                <form action="">
+                    <input 
+                        type="text"
+                        defaultValue="https://www.figma.com/file/NxbZm3CAovYh89dFXe7EOw/Image-Uploader?node-id=1%3A52"
+                        name="link"
+                        ref={ refInput }
+                    />
+                    <button className="link-btn" type="button" onClick={ handleCopyLink }>
+                        Copy Link
+                    </button>
+                </form>
+            </div>
+        </div>
+        
+    );
+};
